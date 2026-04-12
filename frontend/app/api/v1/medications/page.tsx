23bd552d1a845a3340680c10ad349b0c1dc6526e
mitar-j <jankovicmitar04@gmail.com>
2026-04-12 23:25:04 +0200

    staticki postavljeni podaci za lijekove i popularne lijekove

--- frontend/app/api/v1/medications/page.tsx
+++ frontend/app/api/v1/medications/page.tsx
@@ -1,5 +1,57 @@
-import { Header } from "@/app/_components/header";
+"use client";
 
-export default function Home() {
-    return <div></div>
+
+type Medicine = {
+  id: number;
+  name: string;
+  description: string;
+  doses: string[];
+  
+};
+
+const popularMedicines = [
+  "Paracetamol",
+  "Ibuprofen",
+  "Amoksicilin",
+  "Aspirin",
+  "Brufen",
+  "Andol",
+  "Panklav",
+];
+
+const medicines: Medicine[] = [
+  {
+    id: 1,
+    name: "Paracetamol",
+    description:
+      "Analgetik i antipiretik za ublažavanje bola i snižavanje temperature.",
+    doses: ["Sve", "100 mg", "200 mg", "500 mg", "1000 mg"],
+},
+  {
+    id: 2,
+    name: "Paracetamol Extra",
+    description: "Paracetamol sa kofeinom za pojačano djelovanje protiv bola.",
+    doses: ["Sve", "500 mg"],
+},
+  {
+    id: 3,
+    name: "Ibuprofen",
+    description: "Nesteroidni antiinflamatorni lijek za bol i upale.",
+    doses: ["Sve", "200 mg", "400 mg"],
+},
+  {
+    id: 4,
+    name: "Brufen",
+    description: "Lijek za ublažavanje bolova, upala i povišene temperature.",
+    doses: ["Sve", "200 mg", "400 mg", "600 mg"],
+  },
+];
+
+export default function MedicationsSearchPage() {
+  
+  return (
+    <div>
+      
+    </div>
+  );
 }
