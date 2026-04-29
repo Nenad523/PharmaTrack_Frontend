b702538f7a1dc24065c6b9684a5d4d3730c5b0a1
RoninDev <npopo4321@gmail.com>
2026-04-29 16:55:32 +0200

    Deployment

--- frontend/app/api/v1/home/page.tsx
+++ frontend/app/api/v1/home/page.tsx
@@ -3,6 +3,7 @@ import HomeHero from "@/app/_components/Home/hero";
 import HomeNews from "@/app/_components/Home/news";
 import HomeQuickActions from "@/app/_components/Home/quick_actions";
 
+//imports
 export default function Home() {
   return (
     <div>
